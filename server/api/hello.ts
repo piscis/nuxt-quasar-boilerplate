c8cd4cdb2336a8b5dfafe1e2970b68f44132a0ef
Alexander Pirsig <self@pirsig.net>
2021-10-30 22:05:03 +0200

    start working on nestjs integration

--- server/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'http'
-
-export default async (req: IncomingMessage, res: ServerResponse) => {
-  res.statusCode = 200
-  return {foo: "123"}
-}
