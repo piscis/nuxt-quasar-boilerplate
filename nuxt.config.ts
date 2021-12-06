b1256ff2028d67ab40b5fb479c59842133c86bf8
Alexander Pirsig <self@pirsig.net>
2021-12-06 16:28:38 +0100

    upgrade quasar and dependency integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,29 @@
-import { defineNuxtConfig } from 'nuxt3'
-// import vue from '@vitejs/plugin-vue'
-import { quasar } from '@quasar/vite-plugin'
+import { defineNuxtConfig } from "nuxt3";
+//import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 export default defineNuxtConfig({
   ssr: false,
   css: [
-    '~/assets/styles/quasar.sass',
-    '@quasar/extras/roboto-font/roboto-font.css',
-    '@quasar/extras/material-icons/material-icons.css',
+    "~/assets/styles/quasar.sass",
+    "@quasar/extras/roboto-font/roboto-font.css",
+    "@quasar/extras/material-icons/material-icons.css",
   ],
+  build: {
+    transpile: ["quasar"],
+  },
   vite: {
     //define: {
     //  __QUASAR_SSR__: true,
     //  __QUASAR_SSR_SERVER__: true
     //},
     plugins: [
+      //vue({
+        // template: { transformAssetUrls },
+      //}),
       quasar({
-        sassVariables: 'assets/styles/quasar.variables.sass',
-      })
-    ]
-  }
-})
+        sassVariables: "assets/styles/quasar.variables.sass",
+      }),
+    ],
+  },
+});
