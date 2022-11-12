d13fb04d9d5d9369409455cc6a720e05e4f86721
Alex <gh@piscis.dev>
2022-11-12 19:16:49 +0100

    Restructures App  (#4)
    * restructure app
    
    * adds vueuse core
    
    * added vueuse core

---
+++ components/LeftDrawer.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-drawer
+    v-model="data"
+    :key="data ? 'open' : 'close'"
+    bordered
+    class="bg-grey-2"
+  >
+    <q-list>
+      <q-item-label header>Essential Links</q-item-label>
+      <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+        <q-item-section avatar>
+          <q-icon name="school" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Docs</q-item-label>
+          <q-item-label caption>quasar.dev</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item
+        clickable
+        tag="a"
+        target="_blank"
+        href="https://github.com/quasarframework/"
+      >
+        <q-item-section avatar>
+          <q-icon name="code" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Github</q-item-label>
+          <q-item-label caption>github.com/quasarframework</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+        <q-item-section avatar>
+          <q-icon name="chat" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Discord Chat Channel</q-item-label>
+          <q-item-label caption>chat.quasar.dev</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
+        <q-item-section avatar>
+          <q-icon name="forum" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Forum</q-item-label>
+          <q-item-label caption>forum.quasar.dev</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item
+        clickable
+        tag="a"
+        target="_blank"
+        href="https://twitter.com/quasarframework"
+      >
+        <q-item-section avatar>
+          <q-icon name="rss_feed" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Twitter</q-item-label>
+          <q-item-label caption>@quasarframework</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-drawer>
+  <template></template>
+</template>
+
+<script setup lang="ts">
+
+import { useVModel } from '@vueuse/core';
+
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['update:open']);
+
+const data = useVModel(props, 'open', emit);
+
+
+</script>
