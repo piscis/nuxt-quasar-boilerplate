20a1234f28de07d3c253de3ee58e5d79d4fa44ff
Alex <self@pirsig.net>
2023-02-09 14:44:26 +0100

    remove client only section

--- layouts/complicated.vue
+++ layouts/complicated.vue
@@ -3,35 +3,33 @@ const leftDrawerOpen = useState('open', () => false)
 </script>
 
 <template>
-  <ClientOnly>
-    <q-layout view="lHh Lpr lFf">
-      <q-header
-        :key="leftDrawerOpen ? 'open' : 'close'"
-        elevated
-        class="glossy"
-      >
-        <q-toolbar>
-          <q-btn
-            flat
-            dense
-            round
-            aria-label="Menu"
-            icon="menu"
-            @click="leftDrawerOpen = !leftDrawerOpen"
-          />
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+      :key="leftDrawerOpen ? 'open' : 'close'"
+      elevated
+      class="glossy"
+    >
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          aria-label="Menu"
+          icon="menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
 
-          <q-toolbar-title> Quasar App </q-toolbar-title>
-          <div>Quasar v{{ $q.version }}</div>
-        </q-toolbar>
-      </q-header>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
 
-      <LeftDrawer v-model:open="leftDrawerOpen" />
+    <LeftDrawer v-model:open="leftDrawerOpen" />
 
-      <q-page-container>
-        <q-page>
-          <slot />
-        </q-page>
-      </q-page-container>
-    </q-layout>
-  </ClientOnly>
+    <q-page-container>
+      <q-page>
+        <slot />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
