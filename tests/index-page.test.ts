889da7c4ad7a06e183136749562875f764fd9e8f
Alex <self@pirsig.net>
2023-02-09 14:44:50 +0100

    rename hompage test

--- tests/index-page.test.ts
+++ tests/index-page.test.ts
@@ -1,12 +1,13 @@
 import { beforeAll, describe, expect, it } from 'vitest'
 import { $fetch } from '@nuxt/test-utils'
 
-describe('Homepage', () => {
+describe('Index Page', () => {
   let doc: any
   beforeAll(async () => {
     doc = await $fetch('/')
   })
-  it.skip('Renders the experince section', async () => {
-    expect(doc).toContain('Experience')
+  it('Renders the index page with a hello button', async () => {
+    expect(doc).toContain('q-btn__content')
+    expect(doc).toContain('HELLO')
   })
 })
